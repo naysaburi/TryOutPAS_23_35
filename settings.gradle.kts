0b9dc2b05cf3b711c931d49ffe424125bc269ac2
naysaburi <diggiepier550@gmail.com>
2025-05-19 08:22:19 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex(
+                    "com\\.android.*"
+                )
+                includeGroupByRegex(
+                    "com\\.google.*"
+                )
+                includeGroupByRegex(
+                    "androidx.*"
+                )
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name =
+    "TryOutPAS_23_35"
+include(
+    ":app"
+)
