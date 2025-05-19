8490de7f1f87a697a09944eba69b11ef391a670d
naysaburi <diggiepier550@gmail.com>
2025-05-19 09:25:58 +0700

    Hehehe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 
 android {
     namespace =
-        "com.example.belajarpas2"
+        "com.example.tryoutpas_23_35"
     compileSdk =
         35
 
     defaultConfig {
         applicationId =
-            "com.example.belajarpas2"
+            "com.example.tryoutpas_23_35"
         minSdk =
             27
         targetSdk =
@@ -60,30 +60,6 @@ dependencies {
     implementation(
         libs.constraintlayout
     )
-
-    // Retrofit
-    implementation(
-        "com.squareup.retrofit2:retrofit:2.9.0"
-    )
-    implementation(
-        "com.squareup.retrofit2:converter-gson:2.9.0"
-    )
-    implementation(
-        "androidx.recyclerview:recyclerview:1.3.2"
-    )
-    implementation(
-        "com.google.android.material:material:1.11.0"
-    )
-    implementation(
-        "com.github.bumptech.glide:glide:4.16.0"
-    )
-    implementation(
-        "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
-    )
-    annotationProcessor(
-        "com.github.bumptech.glide:compiler:4.16.0"
-    )
-
     testImplementation(
         libs.junit
     )
@@ -93,4 +69,5 @@ dependencies {
     androidTestImplementation(
         libs.espresso.core
     )
+
 }
